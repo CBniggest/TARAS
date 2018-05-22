c2ee97c613ecb301cc58041ea0c43f14f3733e2d
CBniggest <39080397+CBniggest@users.noreply.github.com>
2018-05-22 09:58:41 +0300

    Add files via upload

--- Assets/PLAYRCONTROLLER.cs
+++ Assets/PLAYRCONTROLLER.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PLAYRCONTROLLER : MonoBehaviour {
 
     private Rigidbody rb;
+    public Text scoreText;
 
 
+    private int score;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-
+        score = 0;
+        scoreText.text = "Score: " + score.ToString();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +24,22 @@ public class PLAYRCONTROLLER : MonoBehaviour {
         float h = Input.GetAxis("Horizontal");
         Vector3 dir = new Vector3(h, 0, v);
 
+
         rb.AddForce(dir);
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("tag pickap"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            scoreText.text = "Score: " + score.ToString();
+        }
+    }
+
+    
+
+
+
 }
